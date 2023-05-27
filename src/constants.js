17c530bad4885f87276c8108451eb143e0b3f7e6
MohdSuhail786 <thesuhailansari786246@gmail.com>
2023-05-28 00:29:45 +0530

    build update

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
 export default {
-    BASE_API: 'http://localhost:8080/api',
-    // BASE_API: 'https://vipcutlery.onrender.com/api',
+    // BASE_API: 'http://localhost:8080/api',
+    BASE_API: 'https://vipcutlery.onrender.com/api',
 }
